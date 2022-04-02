c6b4fc5901e15f5a68b8c04912b34fdf9c2527b9
Hugo Targino <hg.targino@gmail.com>
2022-04-02 13:46:56 -0300

    deposit account

--- src/index.js
+++ src/index.js
@@ -43,4 +43,20 @@ app.get("/statement", verifyExistsAccountByCPF, (request, response) => {
   return response.json(customer.statement);
 });
 
+app.post("/deposit", verifyExistsAccountByCPF, (request, response) => {
+  const { customer } = request;
+  const {description, amount } =  request.body;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  };
+
+  customer.statement.push(statementOperation);
+  
+  return response.status(201).send();
+});
+
 app.listen(3333);
